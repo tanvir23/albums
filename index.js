089ef581a8eab28d184d81039f77343e3abfbd81
Tanvir <tanvir@nltvc.com>
2018-06-06 16:56:31 +0800

    added first component and register it for rendering it on screen

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import library
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
 
+// create component
+const App = () => (
+    <Text>
+        Hello World
+    </Text>
+);
+   
+// render it to device
 AppRegistry.registerComponent('albums', () => App);
